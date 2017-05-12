1fda5b4d06eafb79f4f697bb7521e3edec95d3e0
muthuraman-s <muthuraman.sethuramalingam@mindtree.com>
2017-05-12 16:34:40 +0530

    Node JS module three commits

---
+++ app.js
@@ -0,0 +1,24 @@
+var express=require('express');
+var exphbs=require('express-handlebars');
+var routes=require('./routes/route.js');
+
+var app=express();
+
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout:'main'}));
+
+app.get('/',routes.home);
+app.get('/:hotel',routes.hotel);
+
+//localhost port details.
+//var port = process.env.PORT || 8080;
+
+//Heroku port changed.
+var port = process.env.PORT || 8085;
+
+var server=app.listen(port,function(req,res){
+    console.log('Catch the action at http://localhost:'+port);
+});
+module.exports = app;
